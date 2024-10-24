9e398eb2ed352fc911c7b502201d47647d3fa2dc
docnite <docnite0530@gmail.com>
2024-10-24 12:51:48 +0300

    Added polkit-gnome

--- configuration.nix
+++ configuration.nix
@@ -134,6 +134,9 @@
    # Screenshots
    grim slurp
 
+   # Polkit 
+   polkit_gnome
+
    # gsettings
    gsettings-desktop-schemas 
   ];
@@ -261,6 +264,23 @@
     ];
   };
 
+  # Polkit from old config
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
+
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -331,6 +351,26 @@
   # List some security features:
 
   security.rtkit.enable = true;
+
+  # Enable polkit
+  security.polkit.enable = true;
+
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (
+        subject.isInGroup("users")
+          && (
+            action.id == "org.freedesktop.login1.reboot" ||
+            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.power-off" ||
+            action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+          )
+        )
+      {
+        return polkit.Result.YES;
+      }
+    })
+  '';
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
